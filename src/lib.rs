418188f47fa36758b66d267f100ca8f281e4b0ae
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2020-08-30 07:07:47 +0200

    Fixed a doc test

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ mod error;
 pub use crate::{char::Char, digit::Digit, error::Error};
 
 use ::embedded_hal::blocking::i2c::{Write, WriteRead};
+use ::enum_iterator::IntoEnumIterator;
 use ::extend::ext;
 pub use ::ht16k33::*; // TODO Replace with proper prelude
 
@@ -50,7 +51,7 @@ where
     ///
     /// assert_eq!(phat.display_buffer()[4..6], [DisplayData::from_byte(0), DisplayData::from_byte(0b0000_0111)]);
     /// ```
- 
+
     /// Update digit buffer
     ///
     /// # Arguments
@@ -97,13 +98,13 @@ where
     /// # let mut phat = PHat::new(I2c::new(), 0u8);
     /// # phat.initialize();
     /// phat.set_digit(Digit::DIGIT_3, Char::from('*'));
-    /// assert_eq!(phat.display_buffer()[6..8], [DisplayData::from_byte(0b0011_1111), DisplayData::from_byte(0b1100_0000)]);
+    /// assert_eq!(phat.display_buffer()[6..8], [DisplayData::from_byte(0b1100_0000), DisplayData::from_byte(0b0011_1111)]);
     ///
     /// phat.set_digit(Digit::DIGIT_3, [0b0011_1111, 0b1100_0000]);
     /// assert_eq!(phat.display_buffer()[6..8], [DisplayData::from_byte(0b0011_1111), DisplayData::from_byte(0b1100_0000)]);
     ///
     /// phat.set_digit(Digit::DIGIT_3, '*');
-    /// assert_eq!(phat.display_buffer()[6..8], [DisplayData::from_byte(0b0011_1111), DisplayData::from_byte(0b1100_0000)]);
+    /// assert_eq!(phat.display_buffer()[6..8], [DisplayData::from_byte(0b1100_0000), DisplayData::from_byte(0b0011_1111)]);
     /// ```
     fn set_digit<T>(&mut self, digit: Digit, t: T) -> Result<E>
     where
@@ -117,8 +118,8 @@ where
             .try_for_each(|(c, d)| self.set_row_mask(d, c))
     }
 
-    /// Print a 
-    /// 
+    /// Print a
+    ///
     /// ```
     ///
     /// ```
@@ -127,6 +128,8 @@ where
         T: Into<Char>,
         I: IntoIterator<Item = T>,
     {
+        let offset = Digit::into_enum_iter().skip(offset as usize).flatten();
+
         iter.into_iter()
             .flat_map(T::into)
             .zip(offset)
@@ -159,7 +162,6 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use core::iter::once;
     use ht16k33::i2c_mock::{I2cMock as I2c, I2cMockError as Error};
 
     #[test]
@@ -169,7 +171,7 @@ mod tests {
         phat0.set_digit(Digit::DIGIT_1, 'E')?;
         phat0.set_digit(Digit::DIGIT_2, 'S')?;
         phat0.set_digit(Digit::DIGIT_3, 'T')?;
-        
+
         let mut phat1 = PHat::new(I2c::new(), 0u8);
         phat1.print("TEST".chars())?;
 
