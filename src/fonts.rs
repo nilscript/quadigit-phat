281323b934ca239ff75c0bfadffe1eb78b0edd71
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2023-01-14 11:08:43 +0100

    Lots of improvements (?)

--- src/fonts.rs
+++ src/fonts.rs
@@ -20,235 +20,260 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+use std::{iter::Peekable, ops::BitOr};
+
 use dyn_iter::DynIter;
+use itertools::Itertools;
 use phf::phf_map;
 
-pub mod mask {
-    pub const DOT: u16 = 0b0100_0000_0000_0000;
-    pub const QUESTIONMARK: u16 = 0b0001_0000_1000_0011;
+use self::mask::{QUESTION_MARK, PERIOD};
+
+macro_rules! font {
+    [$(($char:expr => $const:ident : $bits:expr)),* $(,)*] => {
+        /// TODO: Insert documentation
+        pub mod mask {
+            $(pub const $const: u16 = $bits;)*
+        }
+
+        /// TODO: Insert good documentation
+        pub static ASCII: phf::Map<u8, u16> = phf_map! {
+            $($char => mask::$const,)*
+        };
+    };
 }
 
-/// TODO Insert GOOD DOCUMENTATION
-pub static ASCII: phf::Map<u8, u16> = phf_map! {
-    b' ' => 0b0000_0000_0000_0000,
-    b'!' => 0b0000_0000_0000_0110,
-    b'"' => 0b0000_0010_0010_0000,
-    b'#' => 0b0001_0010_1100_1110,
-    b'$' => 0b0001_0010_1110_1101,
-    b'%' => 0b0000_1100_0010_0100,
-    b'&' => 0b0010_0011_0101_1101,
-    b'\''=> 0b000_0010_0000_00000,
-    b'(' => 0b0010_0100_0000_0000,
-    b')' => 0b0000_1001_0000_0000,
-    b'*' => 0b0011_1111_1100_0000,
-    b'+' => 0b0001_0010_1100_0000,
-    b',' => 0b0000_1000_0000_0000,
-    b'-' => 0b0000_0000_1100_0000,
-    b'.' => mask::DOT,
-    b'/' => 0b0000_1100_0000_0000,
-    b'0' => 0b0000_1100_0011_1111,
-    b'1' => 0b0000_0000_0000_0110,
-    b'2' => 0b0000_0000_1101_1011,
-    b'3' => 0b0000_0000_1000_1111,
-    b'4' => 0b0000_0000_1110_0110,
-    b'5' => 0b0010_0000_0110_1001,
-    b'6' => 0b0000_0000_1111_1101,
-    b'7' => 0b0000_0000_0000_0111,
-    b'8' => 0b0000_0000_1111_1111,
-    b'9' => 0b0000_0000_1110_1111,
-    b':' => 0b0001_0010_0000_0000,
-    b';' => 0b0000_1010_0000_0000,
-    b'<' => 0b0010_0100_0000_0000,
-    b'=' => 0b0000_0000_1100_1000,
-    b'>' => 0b0000_1001_0000_0000,
-    b'?' => 0b0001_0000_1000_0011,
-    b'@' => 0b0000_0010_1011_1011,
-    b'A' => 0b0000_0000_1111_0111,
-    b'B' => 0b0001_0010_1000_1111,
-    b'C' => 0b0000_0000_0011_1001,
-    b'D' => 0b0001_0010_0000_1111,
-    b'E' => 0b0000_0000_1111_1001,
-    b'F' => 0b0000_0000_0111_0001,
-    b'G' => 0b0000_0000_1011_1101,
-    b'H' => 0b0000_0000_1111_0110,
-    b'I' => 0b0001_0010_0000_0000,
-    b'J' => 0b0000_0000_0001_1110,
-    b'K' => 0b0010_0100_0111_0000,
-    b'L' => 0b0000_0000_0011_1000,
-    b'M' => 0b0000_0101_0011_0110,
-    b'N' => 0b0010_0001_0011_0110,
-    b'O' => 0b0000_0000_0011_1111,
-    b'P' => 0b0000_0000_1111_0011,
-    b'Q' => 0b0010_0000_0011_1111,
-    b'R' => 0b0010_0000_1111_0011,
-    b'S' => 0b0000_0000_1110_1101,
-    b'T' => 0b0001_0010_0000_0001,
-    b'U' => 0b0000_0000_0011_1110,
-    b'V' => 0b0000_1100_0011_0000,
-    b'W' => 0b0010_1000_0011_0110,
-    b'X' => 0b0010_1101_0000_0000,
-    b'Y' => 0b0001_0101_0000_0000,
-    b'Z' => 0b0000_1100_0000_1001,
-    b'[' => 0b0000_0000_0011_1001,
-    b'\\'=> 0b001_0000_1000_00000,
-    b']' => 0b0000_0000_0000_1111,
-    b'^' => 0b0000_1100_0000_0011,
-    b'_' => 0b0000_0000_0000_1000,
-    b'`' => 0b0000_0001_0000_0000,
-    b'a' => 0b0001_0000_0101_1000,
-    b'b' => 0b0010_0000_0111_1000,
-    b'c' => 0b0000_0000_1101_1000,
-    b'd' => 0b0000_1000_1000_1110,
-    b'e' => 0b0000_1000_0101_1000,
-    b'f' => 0b0000_0000_0111_0001,
-    b'g' => 0b0000_0100_1000_1110,
-    b'h' => 0b0001_0000_0111_0000,
-    b'i' => 0b0001_0000_0000_0000,
-    b'j' => 0b0000_0000_0000_1110,
-    b'k' => 0b0011_0110_0000_0000,
-    b'l' => 0b0000_0000_0011_0000,
-    b'm' => 0b0001_0000_1101_0100,
-    b'n' => 0b0001_0000_0101_0000,
-    b'o' => 0b0000_0000_1101_1100,
-    b'p' => 0b0000_0001_0111_0000,
-    b'q' => 0b0000_0100_1000_0110,
-    b'r' => 0b0000_0000_0101_0000,
-    b's' => 0b0010_0000_1000_1000,
-    b't' => 0b0000_0000_0111_1000,
-    b'u' => 0b0000_0000_0001_1100,
-    b'v' => 0b0010_0000_0000_0100,
-    b'w' => 0b0010_1000_0001_0100,
-    b'x' => 0b0010_1000_1100_0000,
-    b'y' => 0b0010_0000_0000_1100,
-    b'z' => 0b0000_1000_0100_1000,
-    b'{' => 0b0000_1001_0100_1001,
-    b'|' => 0b0001_0010_0000_0000,
-    b'}' => 0b0010_0100_1000_1001,
-    b'~' => 0b0000_0101_0010_0000,
-};
+font! {
+    (b' ' => SPACE:                 0b0000_0000_0000_0000),
+    (b'!' => EXCLAMATION_POINT:     0b0000_0000_0000_0110),
+    (b'"' => QUOTATION_MARK:        0b0000_0010_0010_0000),
+    (b'#' => NUMBER_SIGN:           0b0001_0010_1100_1110),
+    (b'$' => DOLLAR_SIGN:           0b0001_0010_1110_1101),
+    (b'%' => PERCENT_SIGN:          0b0000_1100_0010_0100),
+    (b'&' => AMPERSAND:             0b0010_0011_0101_1101),
+    (b'\''=> APOSTROPHE:            0b0000_0100_0000_0000),
+    (b'(' => LEFT_PARENTHESIS:      0b0010_0100_0000_0000),
+    (b')' => RIGHT_PARENTHESIS:     0b0000_1001_0000_0000),
+    (b'*' => ASTERISK:              0b0011_1111_1100_0000),
+    (b'+' => PLUS_SIGN:             0b0001_0010_1100_0000),
+    (b',' => COMMA:                 0b0000_1000_0000_0000),
+    (b'-' => HYPHEN_MINUS:          0b0000_0000_1100_0000),
+    (b'.' => PERIOD:                0b0100_0000_0000_0000),
+    (b'/' => SLASH:                 0b0000_1100_0000_0000),
+    (b'0' => DIGIT_ZERO:            0b0000_1100_0011_1111),
+    (b'1' => DIGIT_ONE:             0b0000_0000_0000_0110),
+    (b'2' => DIGIT_TWO:             0b0000_0000_1101_1011),
+    (b'3' => DIGIT_THREE:           0b0000_0000_1000_1111),
+    (b'4' => DIGIT_FOUR:            0b0000_0000_1110_0110),
+    (b'5' => DIGIT_FIVE:            0b0010_0000_0110_1001),
+    (b'6' => DIGIT_SIX:             0b0000_0000_1111_1101),
+    (b'7' => DIGIT_SEVEN:           0b0000_0000_0000_0111),
+    (b'8' => DIGIT_EIGHT:           0b0000_0000_1111_1111),
+    (b'9' => DIGIT_NINE:            0b0000_0000_1110_1111),
+    (b':' => COLON:                 0b0001_0010_0000_0000),
+    (b';' => SEMI_COLON:            0b0000_1010_0000_0000),
+    (b'<' => LESS_THAN_SIGN:        0b0010_0100_0000_0000),
+    (b'=' => EQUALS_SIGN:           0b0000_0000_1100_1000),
+    (b'>' => GREATER_THAN_SIGN:     0b0000_1001_0000_0000),
+    (b'?' => QUESTION_MARK:         0b0001_0000_1000_0011),
+    (b'@' => AT_SIGN:               0b0000_0010_1011_1011),
+    (b'A' => LETTER_A:              0b0000_0000_1111_0111),
+    (b'B' => LETTER_B:              0b0001_0010_1000_1111),
+    (b'C' => LETTER_C:              0b0000_0000_0011_1001),
+    (b'D' => LETTER_D:              0b0001_0010_0000_1111),
+    (b'E' => LETTER_E:              0b0000_0000_1111_1001),
+    (b'F' => LETTER_F:              0b0000_0000_0111_0001),
+    (b'G' => LETTER_G:              0b0000_0000_1011_1101),
+    (b'H' => LETTER_H:              0b0000_0000_1111_0110),
+    (b'I' => LETTER_I:              0b0001_0010_0000_0000),
+    (b'J' => LETTER_J:              0b0000_0000_0001_1110),
+    (b'K' => LETTER_K:              0b0010_0100_0111_0000),
+    (b'L' => LETTER_L:              0b0000_0000_0011_1000),
+    (b'M' => LETTER_M:              0b0000_0101_0011_0110),
+    (b'N' => LETTER_N:              0b0010_0001_0011_0110),
+    (b'O' => LETTER_O:              0b0000_0000_0011_1111),
+    (b'P' => LETTER_P:              0b0000_0000_1111_0011),
+    (b'Q' => LETTER_Q:              0b0010_0000_0011_1111),
+    (b'R' => LETTER_R:              0b0010_0000_1111_0011),
+    (b'S' => LETTER_S:              0b0000_0000_1110_1101),
+    (b'T' => LETTER_T:              0b0001_0010_0000_0001),
+    (b'U' => LETTER_U:              0b0000_0000_0011_1110),
+    (b'V' => LETTER_V:              0b0000_1100_0011_0000),
+    (b'W' => LETTER_W:              0b0010_1000_0011_0110),
+    (b'X' => LETTER_X:              0b0010_1101_0000_0000),
+    (b'Y' => LETTER_Y:              0b0010_1101_0000_0000),
+    (b'Z' => LETTER_Z:              0b0000_1100_0000_1001),
+    (b'[' => LEFT_SQUARE_BRACKET:   0b0000_0000_0011_1001),
+    (b'\\'=> BACKSLASH:             0b0010_0001_0000_0000),
+    (b']' => RIGHT_SQUARE_BRACKET:  0b0000_0000_0000_1111),
+    (b'^' => CARET:                 0b0000_1100_0000_0011),
+    (b'_' => UNDERSCORE:            0b0000_0000_0000_1000),
+    (b'`' => GRAVE_ACCENT:          0b0000_0001_0000_0000),
+    (b'a' => SMALL_LETTER_A:        0b0001_0000_0101_1000),
+    (b'b' => SMALL_LETTER_B:        0b0010_0000_0111_1000),
+    (b'c' => SMALL_LETTER_C:        0b0000_0000_1101_1000),
+    (b'd' => SMALL_LETTER_D:        0b0000_1000_1000_1110),
+    (b'e' => SMALL_LETTER_E:        0b0000_1000_0101_1000),
+    (b'f' => SMALL_LETTER_F:        0b0000_0000_0111_0001),
+    (b'g' => SMALL_LETTER_G:        0b0000_0100_1000_1110),
+    (b'h' => SMALL_LETTER_H:        0b0001_0000_0111_0000),
+    (b'i' => SMALL_LETTER_I:        0b0001_0000_0000_0000),
+    (b'j' => SMALL_LETTER_J:        0b0000_0000_0000_1110),
+    (b'k' => SMALL_LETTER_K:        0b0011_0110_0000_0000),
+    (b'l' => SMALL_LETTER_L:        0b0000_0000_0011_0000),
+    (b'm' => SMALL_LETTER_M:        0b0001_0000_1101_0100),
+    (b'n' => SMALL_LETTER_N:        0b0001_0000_0101_0000),
+    (b'o' => SMALL_LETTER_O:        0b0000_0000_1101_1100),
+    (b'p' => SMALL_LETTER_P:        0b0000_0001_0111_0000),
+    (b'q' => SMALL_LETTER_Q:        0b0000_0100_1000_0110),
+    (b'r' => SMALL_LETTER_R:        0b0000_0000_0101_0000),
+    (b's' => SMALL_LETTER_S:        0b0010_0000_1000_1000),
+    (b't' => SMALL_LETTER_T:        0b0000_0000_0111_1000),
+    (b'u' => SMALL_LETTER_U:        0b0000_0000_0001_1100),
+    (b'v' => SMALL_LETTER_V:        0b0010_0000_0000_0100),
+    (b'w' => SMALL_LETTER_W:        0b0010_1000_0001_0100),
+    (b'x' => SMALL_LETTER_X:        0b0010_1000_1100_0000),
+    (b'y' => SMALL_LETTER_Y:        0b0010_0000_0000_1100),
+    (b'z' => SMALL_LETTER_Z:        0b0000_1000_0100_1000),
+    (b'{' => LEFT_CURLY_BRACKET:    0b0000_1001_0100_1001),
+    (b'|' => VERTICAL_LINE:         0b0001_0010_0000_0000),
+    (b'}' => RIGHT_CURLY_BRACKET:   0b0010_0100_1000_1001),
+    (b'~' => TILDE:                 0b0000_0101_0010_0000),
+}
 
 pub fn try_ascii(c: u8) -> Option<u16> {
     ASCII.get(&c).copied()
 }
 
 pub fn ascii(c: u8) -> u16 {
-    try_ascii(c).unwrap_or(mask::QUESTIONMARK)
+    try_ascii(c).unwrap_or(QUESTION_MARK)
 }
 
 pub fn ascii_iter(i: DynIter<u8>) -> DynIter<u16> {
     DynIter::new(i.map(ascii))
 }
 
-pub struct DotInterpreter<I>
-where
-    I: Iterator<Item = u16>,
+pub struct PeriodInterpreter<I, T>
+where 
+    I: Iterator<Item = T>,
+    T: Copy + BitOr<Output = T> + PartialEq
 {
-    buf: [u16; 3],
-    buf_len: usize,
-    iter: I,
+    iter: Peekable<I>,
+    mask: T,
+    mask_counter: usize,
 }
 
-impl<I> DotInterpreter<I>
-where
-    I: Iterator<Item = u16>,
+impl<I, T> PeriodInterpreter<I, T> 
+where 
+    I: Iterator<Item = T>,
+    T: Copy + BitOr<Output = T> + PartialEq
 {
-    pub fn new(iter: I) -> DotInterpreter<I> {
-        let mut dot = DotInterpreter {
-            buf: [0; 3],
-            buf_len: 0,
-            iter,
-        };
-
-        dot.refill(3);
-        dot
-    }
-
-    fn buf(&self) -> &[u16] {
-        &self.buf[..self.buf_len]
-    }
-
-    fn rotate(&mut self, n: usize) {
-        self.buf.rotate_left(n);
-        self.buf_len -= n;
-    }
-
-    fn refill(&mut self, n: usize) {
-        for i in 0..n {
-            self.iter.next().iter().for_each(|c| {
-                self.buf[3 - n + i] = *c;
-                self.buf_len += 1;
-            });
+    pub fn new(iter: I, mask: T) -> Self {
+        Self {
+            iter: iter.peekable(),
+            mask,
+            mask_counter: 0,
         }
     }
 
-    fn consume(&mut self, n: usize) {
-        self.rotate(n);
-        self.refill(n);
-    }
-
-    fn identity(&mut self, a: u16) -> Option<u16> {
-        self.consume(1);
-        Some(a)
-    }
-
-    fn mask(&mut self, a: u16, b: u16) -> Option<u16> {
-        self.consume(2);
-        Some(a | b)
-    }
-
-    fn escaped(&mut self, a: u16) -> Option<u16> {
-        self.consume(2);
-        Some(a)
+    pub fn from_peekable(iter: Peekable<I>, mask: T) -> Self {
+        Self {
+            iter,
+            mask,
+            mask_counter: 0,
+        }
     }
 }
 
-impl<I> Iterator for DotInterpreter<I>
-where
-    I: Iterator<Item = u16> + Sized,
+impl<I, T> Iterator for PeriodInterpreter<I, T>
+where 
+    I: Iterator<Item = T>,
+    T: Copy + BitOr<Output = T> + PartialEq
 {
-    type Item = u16;
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match *self.buf() {
-            [a, b, c] if b == mask::DOT && c != mask::DOT => self.mask(a, b),
-            [a, b, c] if b == mask::DOT && c == mask::DOT => self.escaped(a),
-            [a, b, _] if b != mask::DOT => self.identity(a),
-            [a, b] if b != mask::DOT => self.identity(a),
-            [a, b] if b == mask::DOT => self.mask(a, b),
-            [a] => self.identity(a),
-            [..] => None, //
+        if self.mask_counter != 0 {
+            self.mask_counter -= 2;
+            return Some(self.mask);
+        }
+
+        let next = self.iter.next();
+        self.mask_counter = self.iter.peeking_take_while(|b| *b == self.mask)
+            .count();
+            
+        if self.mask_counter % 2 == 1 {
+            self.mask_counter -= 1;
+            next.map(|n| n | self.mask)
+        } else { 
+            next
         }
     }
 }
 
-pub fn dot_iter(i: DynIter<u8>) -> DynIter<u16> {
-    DynIter::new(DotInterpreter::new(i.map(ascii)))
+pub fn period_iter(i: DynIter<u8>) -> impl Iterator<Item = u16> + '_ {
+    PeriodInterpreter::new(DynIter::new(i.map(ascii).peekable()), PERIOD)
 }
 
 #[cfg(test)]
 mod tests {
+    use assert_hex::assert_eq_hex;
+    use crate::fonts::mask::{DIGIT_ZERO, PERIOD, DIGIT_ONE, DIGIT_TWO, DIGIT_THREE};
+
     use super::*;
 
     #[test]
-    fn dot_interpreter() {
-        assert_eq!(
-            vec![0x0C3F, 0x4C3F, 0x0C3F, 0x0C3F],
-            dot_iter(DynIter::new("00.00".bytes())).collect::<Vec<u16>>()
+    fn dot_interpreter_unescaped() {
+        assert_eq_hex!(
+            vec![DIGIT_ZERO, DIGIT_ONE | PERIOD, DIGIT_TWO, DIGIT_THREE],
+            period_iter(DynIter::new("01.23".bytes())).collect::<Vec<u16>>()
         );
+    }
 
-        assert_eq!(
-            vec![0x0C3F, 0x0C3F, mask::DOT, 0x0C3F],
-            dot_iter(DynIter::new("00..0".bytes())).collect::<Vec<u16>>()
+    #[test]
+    fn dot_interpreter_escaped() {
+        assert_eq_hex!(
+            vec![DIGIT_ZERO, DIGIT_ONE, PERIOD, DIGIT_TWO],
+            period_iter(DynIter::new("01..2".bytes())).collect::<Vec<u16>>()
         );
+    }
 
-        assert_eq!(
-            vec![0x0C3F, 0x0C3F, mask::DOT, 0x0C3F],
-            dot_iter(DynIter::new("00...0".bytes())).collect::<Vec<u16>>()
+    #[test]
+    fn dot_interpreter_escaped_unescaped() {
+        assert_eq_hex!(
+            vec![DIGIT_ZERO, DIGIT_ONE | PERIOD, PERIOD, DIGIT_TWO],
+            period_iter(DynIter::new("01...2".bytes())).collect::<Vec<u16>>()
         );
+    }
 
-        assert_eq!(
-            vec![0x0C3F, 0x0C3F, mask::DOT, mask::DOT],
-            dot_iter(DynIter::new("00....".bytes())).collect::<Vec<u16>>()
+    #[test]
+    fn dot_interpreter_escaped_escaped() {
+        assert_eq_hex!(
+            vec![DIGIT_ZERO, DIGIT_ONE, PERIOD, PERIOD],
+            period_iter(DynIter::new("01....".bytes())).collect::<Vec<u16>>()
         );
+    }
+
+    #[test]
+    fn dot_interpreter_three_dots() {
+        assert_eq_hex!(
+            vec![PERIOD, PERIOD],
+            period_iter(DynIter::new("...".bytes())).collect::<Vec<u16>>()
+        );
+    }
+
+    #[test]
+    fn dot_interpreter_four_dots() {
+        assert_eq_hex!(
+            vec![PERIOD, PERIOD],
+            period_iter(DynIter::new("....".bytes())).collect::<Vec<u16>>()
+        );
+    }
+
+    #[test]
+    fn dot_interpreter_period_followed() {
+        assert_eq_hex!(
+            vec![PERIOD, DIGIT_ONE],
+            period_iter(DynIter::new(".1".bytes())).collect::<Vec<u16>>()
+        )
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,22 @@
 pub mod fonts;
 
-use core::mem;
+use core::iter::once;
+use ambassador::{Delegate};
 use dyn_iter::DynIter;
 use embedded_hal::blocking::i2c::{Read, Write};
-pub use ht16k33_lite::prelude::*;
-use std::iter::once;
+use ht16k33_lite::*;
 
 /// Number of characters that can be displayed at once.
 pub const CHAR_TOTAL: usize = 4;
 /// Number of bytes needed to represent a character.
 pub const CHAR_SIZE: usize = 2;
 
-pub type QuadBuf = [u16; CHAR_TOTAL];
-pub type HT16K33Buf = [u8; COMMONS_SIZE];
-
-/// Writes a single character bitmask to address in buffer
-pub fn write_char(dbuf: &mut QuadBuf, addr: usize, c: u16) {
-    dbuf[addr] = c;
-}
-
-/// 
-pub fn write_dot(dbuf: &mut QuadBuf, addr: usize, dot: bool, dotmask: u16) {
-    match dot {
-        true  => dbuf[addr] |=  dotmask,
-        false => dbuf[addr] &= !dotmask,
-    }
-}
-
-pub fn write_str(dbuf: &mut QuadBuf, iter: &mut dyn Iterator<Item = u16>) {
-    iter.take(CHAR_TOTAL).enumerate().for_each(|(i, c)| dbuf[i] = c)
-}
-
-pub fn toggle_dot(dbuf: &mut QuadBuf, addr: usize, dotmask: u16) {
-    dbuf[addr] ^= dotmask
-}
-
-pub fn flush<I2C, E>(ht16k33: &mut HT16K33<I2C>, dbuf: &QuadBuf) -> Result<E>
-where
-    I2C: Read<Error = E> + Write<Error = E>,
-{
-    unsafe {
-        ht16k33.write_dram(
-            &DisplayDataAddressPointer::P0,
-            &mem::transmute::<QuadBuf, HT16K33Buf>(*dbuf),
-        )
-    }
-}
-
+#[derive(Delegate)]
+#[delegate(HT16K33Trait<I2C, E>, target = "ht16k33")]
 pub struct PHat<I2C, E>
 where
     I2C: Read<Error = E> + Write<Error = E>,
 {
-    dbuf: QuadBuf, // Display buffer
     font: fn(DynIter<u8>) -> DynIter<u16>,
     ht16k33: HT16K33<I2C>,
 }
@@ -65,112 +30,65 @@ where
         -> std::result::Result<PHat<I2C, E>, E> 
     {
         let ht16k33 = HT16K33::new(i2c, addr);
-        Ok(PHat {
-            dbuf: [0; CHAR_TOTAL],
-            font,
-            ht16k33,
-        })
+        Ok(PHat { font, ht16k33 })
+    }
+    
+    pub fn cbuf(&self) -> &[u16] {
+        unsafe { self.dbuf().align_to().1 }
+    }
+
+    pub fn cbuf_mut(&mut self) -> &mut [u16] {
+        unsafe { self.dbuf_mut().align_to_mut().1 }
+    }
+
+    pub fn set_cbuf(&mut self, src: &[u16; CHAR_TOTAL]) {
+        self.cbuf_mut().copy_from_slice(src);
     }
 
     /// Writes single character at address in display buffer.
-    pub fn write_char(&mut self, addr: usize, c: u8) {
-        write_char(
-            &mut self.dbuf, 
-            addr, 
-            (self.font)(DynIter::new(once(c))).next().unwrap()
-        )
+    pub fn set_char(&mut self, addr: usize, c: u8) {
+        self.cbuf_mut()[addr] = (self.font)(DynIter::new(once(c)))
+            .next()
+            .unwrap();
+        
     }
 
     /// Writes dot to display buffer 
     /// using `self.font` to map character '.' into a bitmask.
-    pub fn write_dot(&mut self, addr: usize, dot: bool) {
-        write_dot(
-            &mut self.dbuf, 
-            addr, 
-            dot, 
-            (self.font)(DynIter::new(once(b'.'))).next().unwrap()
-        )
+    pub fn set_dot(&mut self, addr: usize, dot: bool) {
+        let dotmask = self.font()(DynIter::new(once(b'.'))).next().unwrap();
+        let cbuf = self.cbuf_mut();
+
+        match dot {
+            false => cbuf[addr] |=  dotmask, 
+            true  => cbuf[addr] &= !dotmask,
+        }
     }
 
     /// Writes string reference to display buffer using `self.font` as map.
-    pub fn write_str(&mut self, s: &str) {
-        write_str(&mut self.dbuf, &mut (self.font)(DynIter::new(s.bytes())))
+    pub fn set_str(&mut self, s: &str) {
+        self.font()(DynIter::new(s.bytes()))
+            .zip(self.cbuf_mut().iter_mut())
+            .for_each(|(f, c)| *c = f);
+    }
+
+    pub fn font(&self) -> fn(DynIter<u8>) -> DynIter<u16> {self.font}
+
+    pub fn set_font(&mut self, font: fn(DynIter<u8>) -> DynIter<u16>) {
+        self.font = font
     }
 
     /// Xors the bitmask of character '.' 
     /// at address in display buffer.
     pub fn toggle_dot(&mut self, addr: usize) {
-        toggle_dot(
-            &mut self.dbuf, 
-            addr, 
-            (self.font)(DynIter::new(once(b'.'))).next().unwrap()
-        )
+        self.cbuf_mut()[addr] ^= (self.font)(DynIter::new(once(b'.'))).next().unwrap();
     }
 
-    /// Returns internal display buffer as a reference.
-    pub fn dbuf(&self) -> &QuadBuf {&self.dbuf}
-
-    /// Returns internal display buffer as a mutable reference.
-    pub fn mut_dbuf(&mut self) -> &mut QuadBuf {&mut self.dbuf}
-
     /// Flushes display buffer, writing everything to i2c interface.
-    pub fn flush(&mut self) -> Result<E> {flush(&mut self.ht16k33, &self.dbuf)}
-
-    // Methods below are mostly reexported from ht16k33
-    
-    /// Turns system to normal mode, set's Row/Int to Row, turns on display, 
-    /// turns dimming to max, and writes a blank dbuf.
-    pub fn power_on(&mut self) -> Result<E> {self.ht16k33.power_on()}
-
-    /// Writes blank buffer, turns off display and oscillator.
-    pub fn shutdown(&mut self) -> Result<E> {self.ht16k33.shutdown()}
+    pub fn flush(&mut self) -> Result<E> {
+        self.ht16k33.write_dbuf()
+    }
 
     /// Destroys self and returns internal ht16k33 interface.
     pub fn take_ht16k33(self) -> HT16K33<I2C> {self.ht16k33}
-
-    /// Returns reference to internat system mode. 
-    pub fn system_setup(&self) -> SystemSetup {self.ht16k33.system_setup()}
-
-    /// Writes new System mode to controller
-    /// and if successful store it's new state.
-    pub fn write_system_setup(&mut self, sys: SystemSetup) -> Result<E> {
-        self.ht16k33.write_system_setup(sys)
-    }
-
-    /// Returns reference to internal display state. 
-    pub fn display_setup(&self) -> DisplaySetup {self.ht16k33.display_setup()}
-    
-    /// Writes a new display state to controller 
-    /// and if successful store it's new state.
-    pub fn write_dimming_setup(&mut self, dpy: DisplaySetup) -> Result<E> {
-        self.ht16k33.write_display_setup(dpy)
-    }
-
-    /// Returns reference to internal rowint state.
-    pub fn row_int_set(&self) -> RowIntSet {self.ht16k33.row_int_set()}
-
-    /// Writes new Row/Int output to controller
-    /// and if successful store it's new state.
-    pub fn write_row_int_set(&mut self, rowint: RowIntSet) -> Result<E> {
-        self.ht16k33.write_row_int_set(rowint)
-    }
-
-    /// Returns dimming level.
-    pub fn dimming_set(&self) -> DimmingSet {self.ht16k33.dimming_set()}
-
-    /// Writes a new dimming level to controller
-    /// and if successful store it's new state.
-    pub fn write_dimming_set(&mut self, dim: DimmingSet) -> Result<E> {
-        self.ht16k33.write_dimming_set(dim)
-    }
-
-    /// Writes a slice of zeros to controller Display Ram.
-    pub fn clear_dram(&mut self) -> Result<E> {self.ht16k33.clear_dram()}
-
-    /// Reads Display Ram from controller into internal display buffer.
-    pub fn read_dram(&mut self) -> Result<E> {
-        self.ht16k33.read_dram(unsafe {
-            &mut mem::transmute::<QuadBuf, HT16K33Buf>(self.dbuf)
-        })
-    }
 }
